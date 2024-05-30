b2e8d47ce200052759f0d110164dcaa808b623cb
Tom Shen <tom.shen.999@gmail.com>
2024-05-30 17:51:02 +0800

    23

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -120,6 +120,13 @@ resource "aws_security_group" "endpoint_access" {
     to_port     = 443
     protocol    = "tcp"
   }
+
+    ingress {
+    cidr_blocks = [aws_vpc.main.cidr_block]
+    from_port   = 5050
+    to_port     = 5050
+    protocol    = "tcp"
+  }
 }
 
 resource "aws_vpc_endpoint" "ecr" {
