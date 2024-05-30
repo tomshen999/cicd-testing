c502234efacd5b62f4320a16f456ff36db9b02e5
Tom Shen <tom.shen.999@gmail.com>
2024-05-30 15:48:12 +0800

    9

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -19,10 +19,10 @@ output "ecr_repo_api_app" {
   value       = aws_ecr_repository.api-app.repository_url
 }
 
-output "ecr_repo_report" {
-  description = "ECR repository URL for ot-report image"
-  value       = aws_ecr_repository.report.repository_url
-}
+# output "ecr_repo_report" {
+#   description = "ECR repository URL for ot-report image"
+#   value       = aws_ecr_repository.report.repository_url
+# }
 
 # output "ecr_repo_proxy" {
 #   description = "ECR repository URL for the proxy image"
